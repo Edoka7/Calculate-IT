e00079bd78da75766cd9a141a228db0af06fc7f2
Paweł Hładoń <pa.hladon@gmail.com>
2022-07-13 21:20:23 +0200

    make special operations options available

--- js/main.js
+++ js/main.js
@@ -21,7 +21,93 @@ const inputNumber = (e) => {
   renderScreen();
 };
 
-const specialOperation = (e) => {};
+const doEqueals = () => {
+  valueX = Number(valueX);
+  valueY = Number(valueY);
+
+  switch (operationType) {
+    case "/":
+      return valueY / valueX;
+      break;
+
+    case "+":
+      return valueX + valueY;
+      break;
+
+    case "-":
+      return valueX - valueY;
+      break;
+
+    case "*":
+      return valueX * valueY;
+      break;
+
+    default:
+      break;
+  }
+};
+
+const specialOperation = (e) => {
+  const clickKey = e.target.dataset.calcspecialkey;
+
+  switch (clickKey) {
+    case "ac":
+      valueX = null;
+      valueY = null;
+      renderScreen();
+      break;
+
+    default:
+      break;
+  }
+
+  if (valueX === null || valueX === undefined) return;
+
+  switch (clickKey) {
+    case "del":
+      valueX = valueX.slice(0, -1);
+
+      break;
+
+    case "div":
+      operationType = "/";
+      valueY = valueX;
+      valueX = null;
+
+      break;
+
+    case "plus":
+      operationType = "+";
+      valueY = valueX;
+      valueX = null;
+
+      break;
+
+    case "minus":
+      operationType = "-";
+      valueY = valueX;
+      valueX = null;
+
+      break;
+
+    case "mul":
+      operationType = "*";
+      valueY = valueX;
+      valueX = null;
+
+      break;
+
+    case "equals":
+      valueX = doEqueals();
+      valueY = null;
+      break;
+
+    default:
+      break;
+  }
+
+  renderScreen();
+};
 
 const eventsBind = () => {
   numberKeys.forEach((key) => {
